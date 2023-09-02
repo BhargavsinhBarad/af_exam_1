0c62a16d91930dd8fe4340733bfbc3faa72240b9
BHARGAVSINH <bhargavsinhbarad29@gmail.com>
2023-09-02 22:10:19 +0530

    change

--- lib/Screens/details.dart
+++ lib/Screens/details.dart
@@ -15,13 +15,29 @@ class _detailsState extends State<details> {
         title: Text("Read News"),
         centerTitle: true,
       ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: EdgeInsets.all(20),
-        child: Text(
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          style: TextStyle(color: Colors.black, fontSize: 20),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              height: 250,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRls7GbEsZqH_jY5Vz2W27SqSbJcOPLEel7cPQRndKSsA&s"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -14,43 +14,45 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "News",
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text(
+          "News",
         ),
-        // body: FutureBuilder(
-        //     future: api_helper.api.getdata(),
-        //     builder: (context, AsyncSnapshot snapshot) {
-        //       if (snapshot.hasError) {
-        //         return Text("${snapshot.error}");
-        //       } else if (snapshot.hasData) {
-        //         api_model? apimodel = snapshot.data;
-        //         return Container(
-        //           child: Text("${apimodel?.hii['data'][0]['title']}"),
-        //         );
-        //       }
-        //       return Container();
-        //     }));
-        body: ListView.builder(
-          itemCount: 8,
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, 'details');
-            },
-            child: Container(
-              margin: EdgeInsets.all(20),
-              height: 250,
-              width: 250,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRls7GbEsZqH_jY5Vz2W27SqSbJcOPLEel7cPQRndKSsA&s"),
-                      fit: BoxFit.cover)),
+        centerTitle: true,
+      ),
+      // body: FutureBuilder(
+      //     future: api_helper.api.getdata(),
+      //     builder: (context, AsyncSnapshot snapshot) {
+      //       if (snapshot.hasError) {
+      //         return Text("${snapshot.error}");
+      //       } else if (snapshot.hasData) {
+      //         api_model? apimodel = snapshot.data;
+      //         return Container(
+      //           child: Text("${apimodel?.hii['data'][0]['title']}"),
+      //         );
+      //       }
+      //       return Container();
+      //     }));
+      body: ListView.builder(
+        itemCount: 8,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, 'details');
+          },
+          child: Container(
+            margin: EdgeInsets.all(20),
+            height: 250,
+            width: 250,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRls7GbEsZqH_jY5Vz2W27SqSbJcOPLEel7cPQRndKSsA&s"),
+                  fit: BoxFit.cover),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
